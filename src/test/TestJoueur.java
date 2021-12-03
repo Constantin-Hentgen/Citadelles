adbea965c3a202ab9e1b6194bc5e26c49ff2f4c3
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2021-12-03 21:28:02 +0100

    testJoueur décommenté

--- src/test/TestJoueur.java
+++ src/test/TestJoueur.java
@@ -6,11 +6,10 @@ import modele.Joueur;
 public class TestJoueur {
 	public static void main(String[] args){
 		TestJoueur testJoueur = new TestJoueur();
-		// testJoueur.test1();
-		// testJoueur.test2();
+		testJoueur.test1();
 		testJoueur.test3();
-		// testJoueur.test4();
-		// testJoueur.test5();
+		testJoueur.test4();
+		testJoueur.test5();
 	}
 	
 	public void test1(){
