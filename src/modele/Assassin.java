b1727f79023fdd9166824a705d953dc8a69aca2e
WoodyTheOriginal <mathieu1.guignard@gmail.com>
2021-12-07 16:29:07 +0100

    assassin-v1

--- src/modele/Assassin.java
+++ src/modele/Assassin.java
@@ -1,22 +1,42 @@
 package modele;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Assassin extends Personnage {
+    
+    public Assassin(){
+        super("Assassin", Caracteristiques.ASSASSIN, 1);
+    }
 
-	public Assassin(String nom, String caracteristiques, int rang){
-		super(nom, caracteristiques, rang);
-	}
+    Scanner sc = new Scanner(System.in);
 
-	@Override
-	void utiliserPouvoir() {
-		System.out.println(plateau.getNombrePersonnages());
-		// System.out.println(plateau.getPersonnage(1));
-		// for (int i = 0; i < plateau.getNombrePersonnages(); i++){
-		// 	System.out.println(plateau.getPersonnage(i));
-		// }
-	}
+    @Override
+    public void utiliserPouvoir() {
+        boolean continu = true;
 
-	public static void main(String[] args) {
-		Assassin vilain = new Assassin("valin", "jspCaracteristique", 1);
-		vilain.utiliserPouvoir();
-	}
+        System.out.println("Quel personnage voulez-vous assassiner ?");
+        for (int i = 0; i < plateau.getNombreJoueurs() - 1; i++) {
+            System.out.println(plateau.getPersonnage(i).getRang()+"   "+plateau.getPersonnage(i).getNom());
+        }
+
+        do {
+            try {
+                int i = sc.nextInt();
+                for (int j = 0; j < plateau.getNombreJoueurs(); j++) {
+                    if (i != plateau.getPersonnage(j).getRang() || i == this.getRang()) {
+                        System.out.println("Vous ne pouvez pas vous assassiner !");
+                    }
+                    else{
+                        plateau.getPersonnage(i).setAssassine();
+                        continu = false;
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Veuillez entrer le rang du personnage");
+            }
+        } while (continu);
+        
+    }
+
 }
