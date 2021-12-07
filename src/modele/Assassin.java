36111b91482537b361c6d73b3f1116b371fcf1ae
WoodyTheOriginal <mathieu1.guignard@gmail.com>
2021-12-07 18:03:11 +0100

    Merge branch 'master' of https://github.com/Constantin-Hentgen/Citadelles into Partie-3.1

--- src/modele/Assassin.java
+++ src/modele/Assassin.java
@@ -16,27 +16,30 @@ public class Assassin extends Personnage {
         boolean continu = true;
 
         System.out.println("Quel personnage voulez-vous assassiner ?");
-        for (int i = 0; i < plateau.getNombreJoueurs() - 1; i++) {
+        for (int i = 0; i < plateau.getNombrePersonnages(); i++) {
             System.out.println(plateau.getPersonnage(i).getRang()+"   "+plateau.getPersonnage(i).getNom());
         }
 
         do {
-            try {
-                int i = sc.nextInt();
-                for (int j = 0; j < plateau.getNombreJoueurs(); j++) {
-                    if (i != plateau.getPersonnage(j).getRang() || i == this.getRang()) {
+            for (int j = 0; j < plateau.getNombrePersonnages(); j++) {
+                try {
+                    int i = sc.nextInt();
+                    if (i == this.getRang()) {
                         System.out.println("Vous ne pouvez pas vous assassiner !");
                     }
-                    else{
-                        plateau.getPersonnage(i).setAssassine();
+                    else if(i == plateau.getPersonnage(j).getRang()){
+                        plateau.getPersonnage(j).setAssassine();
                         continu = false;
+                        break;
                     }
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Veuillez entrer le rang du personnage");
+                    else{
+                        System.out.println("Veuillez entrer le rang d'un personnage");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Veuillez entrer le rang du personnage");
+                    sc.next();
+                }            
             }
         } while (continu);
-        
     }
-
 }
