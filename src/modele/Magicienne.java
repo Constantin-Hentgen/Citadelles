0212a94042c9f3d356b594d58fc944c4e8d66859
WoodyTheOriginal <mathieu1.guignard@gmail.com>
2021-12-08 08:46:42 +0100

    creation class magicienne

---
+++ src/modele/Magicienne.java
@@ -0,0 +1,15 @@
+package modele;
+
+public class Magicienne extends Personnage {
+
+    public Magicienne(){
+        super("Magicienne", Caracteristiques.MAGICIENNE, 3);
+    }
+
+    @Override
+    void utiliserPouvoir() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
