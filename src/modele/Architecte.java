c3e656a8ac9b894eb71a7075e74231513237a316
alan-grillo <alan.grillo@orange.fr>
2021-12-17 13:04:24 +0100

    architecte fini

---
+++ src/modele/Architecte.java
@@ -0,0 +1,16 @@
+package modele;
+
+public class Architecte extends Personnage  {
+
+    public Architecte(){
+        super("Architecte",Caracteristiques.ARCHITECTE,7);
+    }
+
+    public void utiliserPouvoir(){
+        for(int i = 0; i<2; i++){
+            Quartier q = this.plateau.getPioche().piocher();
+            this.joueur.ajouterQuartierDansMain(q);
+        }
+    }
+    
+}
