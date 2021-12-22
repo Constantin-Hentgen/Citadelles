06b8db88c04cd3ee1fa1a552a8b0d75263f2b98e
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2021-12-22 18:57:08 +0100

    la destruction fonctionne

--- src/modele/Condottiere.java
+++ src/modele/Condottiere.java
@@ -34,6 +34,7 @@ public class Condottiere extends Personnage {
 		
 		// on s'assure que la réponse apporté sera o ou n
 		Scanner sc = new Scanner(System.in);
+
 		do {
 			// Scanner sc = new Scanner(System.in);
 			choix = sc.next();
@@ -115,23 +116,70 @@ public class Condottiere extends Personnage {
 				}
 			}
 			
-			// faire un feedback sur l'opération : tel quartier a été détruit, il vous reste 1or
+			String nomQuartierADetruire = plateau.getJoueur(choixPersonnage-1).getCite()[choixQuartier-1].getNom();
+
+			// faire un feedback sur l'opération : tel quartier va être détruit, il vous reste 1or
 			System.out.println(
 				"Le personnage choisit pour la destruction est " +  
 				plateau.getPersonnage(choixPersonnage - 1).getNom() +
 				" joué par " + plateau.getJoueur(choixPersonnage - 1).getNom() +
-				", son quartier à détruire est " +  
-				plateau.getJoueur(choixPersonnage-1).getCite()[choixQuartier-1].getNom() + "."
+				", le quartier à détruire est " +  
+				nomQuartierADetruire + "."
 			);
+			
+			// si le nombre de joueurs est
+			int nb = plateau.getNombreJoueurs();
+			Quartier[] cite = plateau.getJoueur(choixPersonnage-1).getCite();
+			int tailleCite = plateau.getJoueur(choixPersonnage-1).getCite().length;
 
-			// DESTRUCTION
+			// COMPTABILISE LE NOMBRE DE QUARTIERS DANS LA CITÉ CONCERNÉE
+			int nbQuartier = 0;
+			for (int i = 0; i < tailleCite; i++) {
+				try{
+					if (!cite[i].equals(null)){
+						nbQuartier ++;
+					}
+				} catch (NullPointerException npe) {}
+			}
+			
+			// message d'erreur dans le cas d'une cité complète :
+				// cité de 7 quartiers ou plus pour les parties de 4 à 7 joueurs, 
+				// ou une cité de 8 quartiers pour les parties à 2, 3 ou 8 joueurs.
+			if (((nb == 2 || nb == 3 || nb == 8 ) && (nbQuartier == 8)) || ((nb >= 4 && nb <= 7) && (nbQuartier == 7))){
+				System.out.println("Nombre de joueurs : " + nb);
+				System.out.println("Nombre de quartiers : " + nbQuartier);
+				System.out.println("Cité complète : impossible de détruire un quartier");
+			} else {
+				// DESTRUCTION
 
-			// CONDITION si la cité du joueur est complète alors je ne peux rien détruire
-			// stocker le quartier concerné
-			// placer le dernier quartier de la cité à la place du quartier détruit
-			// mettre le dernier élément à null
-			// mettre le quartier à la fin de la pioche
-			// retirer COUT-1 au condottiere
+
+				plateau.getJoueur(choixPersonnage-1).getCite()[choixQuartier-1].getNom();
+
+				// System.out.println("AVANT QUE JE TOUCHE À SA CITÉ");
+
+				// for (int i = 0; i < tailleCite; i++) {
+				// 	try{
+				// 		System.out.println(plateau.getJoueur(choixPersonnage-1).getCite()[i].getNom());
+				// 	} catch (NullPointerException npe) {}
+				// }
+
+				// System.out.println("APRÈS QUE J'AI TOUT CASSÉ");
+
+
+				plateau.getJoueur(choixPersonnage-1).retirerQuartierDansCite(nomQuartierADetruire);
+
+				for (int i = 0; i < tailleCite; i++) {
+					try{
+						System.out.println(plateau.getJoueur(choixPersonnage-1).getCite()[i].getNom());
+					} catch (NullPointerException npe) {}
+				}
+
+				// stocker le quartier concerné
+				// placer le dernier quartier de la cité à la place du quartier détruit
+				// mettre le dernier élément à null
+				// mettre le quartier à la fin de la pioche = défausse
+				// retirer COUT-1 au condottiere
+			}
 		}
 		sc.close();
 	}
