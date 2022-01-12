48194f315f47e9f37562e881e129077edd099c59
alan-grillo <alan.grillo@orange.fr>
2022-01-12 16:36:02 +0100

    voyante fini

--- src/modele/Voyante.java
+++ src/modele/Voyante.java
@@ -46,17 +46,58 @@ public class Voyante extends Personnage {
             System.out.println();
             int choix = inter.lireUnEntier(0, pris.size());
             q = pris.get(choix);
+            pris.remove(q);
             System.out.println();
 
             // Choix du joueur
             System.out.println("A qui voulez vous donner " + q.getNom());
             System.out.println();
             for(int i = 0;i<restants.size();i++){
-                System.out.println(i+" | "+ restants.get(i));
+                System.out.println(i+" | "+ restants.get(i).getNom());
                 
             }
 
+            choix = inter.lireUnEntier(0,restants.size());
+            Joueur receveur = restants.get(choix);
+            receveur.ajouterQuartierDansMain(q);
+            restants.remove(receveur);
+            System.out.println();
+
         }
+    }
+
+    public void utiliserPouvoirAvatar(){
+         // Contruction de la liste des autres joueurs
+         ArrayList<Joueur> restants = new ArrayList<Joueur>();
+         for(int i=0;i< plateau.getNombreJoueurs();i++){
+             Joueur j = plateau.getJoueur(i);
+             if(!j.equals(this.joueur)){
+                 restants.add(j);
+             }
+         }
+ 
+         // On prend les cartes au hasard
+         ArrayList<Quartier> pris = new ArrayList<Quartier>();
+         Random r = new Random();
+         for(Joueur j :restants ){
+             int n = j.nbQuartiersDansMain();
+             int i = r.nextInt(n);
+             ArrayList<Quartier> main = j.getMain();
+             pris.add(main.get(i));
+             j.retirerQuartierDansMain(main.get(i));
+         }
+        
+         while(pris.size()!=0){
+            int numeroCarte = r.nextInt(pris.size());
+            int numeroJoueur = r.nextInt(restants.size());
+            Quartier q = pris.get(numeroCarte);
+            Joueur j = restants.get(numeroJoueur);
+            j.ajouterQuartierDansMain(q);
+            pris.remove(q);
+            restants.remove(j);
+
+         }
+
     }
 
     
---
+++ src/test/TestVoyante.java
@@ -0,0 +1,68 @@
+package test;
+
+import java.util.ArrayList;
+
+import modele.*;
+
+public class TestVoyante {
+    public static void main(String[] args){
+        TestVoyante test = new TestVoyante();
+        //test.test1();
+        test.test2();
+    }
+
+    public void test1() {
+		System.out.println("TEST DU CONSTRUCTEUR");
+		PlateauDeJeu plateau = new PlateauDeJeu();
+		Roi roi = new Roi();
+		plateau.ajouterPersonnage(roi);
+		Voyante voyante = new Voyante();
+		plateau.ajouterPersonnage(voyante);
+		Test.test(plateau.getNombrePersonnages()== 2,"nombre de joueurs");
+		Test.test(plateau.getPersonnage(1)==voyante,
+				"récupération du personnage de la voyante");
+		Test.test(plateau.getPersonnage(1).getRang()==3,
+				"récupération du rang");		
+	}
+
+    public void test2(){
+        System.out.println("TEST POUVOIR");
+        PlateauDeJeu plateau = new PlateauDeJeu();
+        Assassin assassin = new Assassin();
+        Voleur voleur = new Voleur();
+        Voyante voyante = new Voyante();
+        plateau.ajouterPersonnage(assassin);
+        plateau.ajouterPersonnage(voleur);
+        plateau.ajouterPersonnage(voyante);
+
+        Joueur joueur1 = new Joueur("Milou");
+		plateau.ajouterJoueur(joueur1);
+		Joueur joueur2 = new Joueur("Billy");
+		plateau.ajouterJoueur(joueur2);
+		Joueur joueur3 = new Joueur("Rantanplan");
+		plateau.ajouterJoueur(joueur3);
+
+        assassin.setJoueur(joueur1);
+        voleur.setJoueur(joueur2);
+        voyante.setJoueur(joueur3);
+
+        Quartier q = new Quartier("temple",Quartier.TYPE_QUARTIERS[0],1); 
+        joueur1.ajouterQuartierDansMain(q);
+		q = new Quartier("prison",Quartier.TYPE_QUARTIERS[1],2); 
+        joueur2.ajouterQuartierDansMain(q);
+		q = new Quartier("palais",Quartier.TYPE_QUARTIERS[2],5);
+        joueur3.ajouterQuartierDansMain(q);
+
+        voyante.utiliserPouvoirAvatar();
+
+        Test.test(joueur1.nbQuartiersDansMain()==1,"le joueur 1 a toujours sa carte dans sa main");
+        Test.test(joueur2.nbQuartiersDansMain()==1,"le joueur 2 a toujours sa carte dans sa main");
+        Test.test(joueur3.getMain().get(0).equals(q),"la voyante a la meme main");
+        ArrayList<Quartier> mainVoyante = new ArrayList<Quartier>();
+        mainVoyante.add(q);
+
+        Test.test(joueur3.getMain().equals(mainVoyante),"la voyante a la meme main");
+
+        
+    }
+}
