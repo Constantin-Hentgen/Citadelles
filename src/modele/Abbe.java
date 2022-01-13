fd3a510a916a32eb5dff6b87290190809f625c35
alan-grillo <alan.grillo@orange.fr>
2022-01-13 15:15:34 +0100

    abbe sans avatar

---
+++ src/modele/Abbe.java
@@ -0,0 +1,104 @@
+package modele;
+
+import java.util.ArrayList;
+
+import controleur.Interaction;
+
+public class Abbe extends Personnage {
+
+    public Abbe(){
+        super("Abbe",Caracteristiques.ABBE,5);
+    }
+    
+    public void percevoirRessourcesSpecifiques(){
+        if(!assassine){
+            int quartierReligieux = 0;
+
+            for(int i = 0; i<this.joueur.nbQuartiersDansCite();i++){
+                if(joueur.getCite()[i].getType().equals("RELIGIEUX")){
+                    quartierReligieux = quartierReligieux +1;
+                }
+            }
+
+            System.out.println("Vous avez " + quartierReligieux + "dans votre cite");
+            if(quartierReligieux > 0){
+                System.out.println("Combien voulez vous d'or ?(Maximum : " +quartierReligieux);
+                Interaction inter = new Interaction();
+                int choixPiece = inter.lireUnEntier(0, quartierReligieux+1);
+                this.joueur.ajouterPieces(choixPiece);
+                System.out.println();
+                System.out.println("Vous piochez "+ (quartierReligieux-choixPiece)+" cartes");
+                for(int i = 0;i<(quartierReligieux-choixPiece);i++){
+                    this.joueur.ajouterQuartierDansMain(plateau.getPioche().piocher());
+                }
+            }
+        }
+    }
+
+    public void utiliserPouvoir(){
+        if(plateau.getNombreJoueurs()>0){
+            ArrayList<Joueur> plusRiche = new ArrayList<Joueur>();
+            try{
+                plusRiche.add(plateau.getJoueur(0));
+            } catch(NullPointerException e){
+                System.out.println("Il n'y a aucun joueur"); // N'est pas censé arriver
+            }
+        
+            //On cherche le joeur le plus riche, ou les plus riches
+            for(int i = 1;i<plateau.getNombreJoueurs();i++){ // On commence a 1 car 0 est notre ^plus riche de base
+                if(plateau.getJoueur(i).tresor()>=plusRiche.get(0).tresor()){
+                    if(plateau.getJoueur(i).tresor() == plusRiche.get(0).tresor()){
+                        plusRiche.add(plateau.getJoueur(i));
+                    }
+                    else{
+                        plusRiche = new ArrayList<Joueur>();
+                        plusRiche.add(plateau.getJoueur(i));
+                    }
+                }
+            }
+
+            // on donne l'or  l'abbe si un joueur est plus riche
+            if(plusRiche.size() == 1 && !plusRiche.get(0).equals(this.joueur)){
+                System.out.println(plusRiche.get(0).getNom()+" est le plus riche.");
+                if(plusRiche.get(0).tresor()>0){
+                    System.out.println("Vous lui prenez 1 pièce d'or");
+                    this.joueur.ajouterPieces(1);
+                    plusRiche.get(0).retirerPieces(1);
+                }
+                else{System.out.println("Le plus riche n'as pas de piece d'or");}
+                
+            }
+            else if (plusRiche.size() ==1 && plusRiche.contains(this.joueur)){
+                System.out.println("Vous etes le plus riche, vous ne pouvez pas prendre de pieces");
+
+            }
+            else{
+                System.out.println("A qui voulez vous prendre 1 piece");
+                System.out.println();
+                for(int i = 0; i<plusRiche.size();i++){
+                    System.out.println(i +"| "+plusRiche.get(i).getNom());
+                }
+                System.out.println();
+
+                Interaction inter = new Interaction();
+                Joueur victime = this.joueur;
+                while(victime.equals(this.joueur)){
+                    int choix = inter.lireUnEntier(0,plusRiche.size());
+                    victime = plusRiche.get(choix);
+                    if(victime.equals(this.joueur)){
+                        System.out.println("Vous ne pouvez pas vous cibler vous meme");
+                    }
+                }
+
+                this.joueur.ajouterPieces(1);
+                victime.retirerPieces(1);
+
+            }
+            
+        }
+
+       
+    }
+    
+    
+}
